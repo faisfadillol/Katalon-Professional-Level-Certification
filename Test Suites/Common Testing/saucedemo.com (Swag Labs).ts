86f4bb2d1516cfd47adf6a83e813ec9cf0ad3b05
faisfadillol <faisfadillol@gmail.com>
2024-08-02 16:58:49 +0700

    Add files via upload

---
+++ Test Suites/Common Testing/saucedemo.com (Swag Labs).ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>saucedemo.com (Swag Labs)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4fec2049-996a-4e8c-8866-1ce17b0636ce</testSuiteGuid>
+   <testCaseLink>
+      <guid>cb7030cd-3472-4ee6-9dc2-85c2f1a491d9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/saucedemo.com (Swag Labs)/001 - Login with Right Username and Password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>242498be-1ec2-46ce-bb83-8c62894b7d34</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/saucedemo.com (Swag Labs)/002 - Login with Wrong Password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4c5bfd61-597a-420c-84b3-39357ec8bf39</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/saucedemo.com (Swag Labs)/003 - Login Success and Redirect To Dashboard</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
